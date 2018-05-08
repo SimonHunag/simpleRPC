ed6ec68a70d7a47cc0f22467908348fa426fe605
SimonHuang <huanghuapeng@kaike.la>
2018-05-08 16:48:27 +0800

    step3.1 service registry

---
+++ simpleRPCserver/src/main/java/cn/simple/service/HelloServiceImpl.java
@@ -0,0 +1,23 @@
+/**
+ * 版权所有@: 杭州铭师堂教育科技发展有限公司
+ * 注意：本内容仅限于杭州铭师堂教育科技发展有限公司内部使用，禁止外泄以及用于其他的商业目的
+ * CopyRight@: 2018 Hangzhou Mistong Educational Technology Co.,Ltd.
+ * All Rights Reserved.
+ * Note:Just limited to use by Mistong Educational Technology Co.,Ltd. Others are forbidden.
+ */
+package cn.simple.service;
+
+import cn.simple.annotation.SRpcService;
+
+/**
+ * HelloServiceImpl
+ *
+ * Created by huapeng.hhp on 2018/5/7.
+ */
+@SRpcService(value = HelloService.class)
+public class HelloServiceImpl implements HelloService {
+	@Override
+	public void sayHi(String name) {
+		System.out.println("Hello " + name);
+	}
+}
